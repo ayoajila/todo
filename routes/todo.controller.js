44b1776f170b833d1b629d1ad47d7d04bab4e0e5
vajila <vicaajila@gmail.com>
2019-12-24 21:04:30 -0500

    This is a real commit message\!\!
    Signed-off-by: vajila <vicaajila@gmail.com>

--- routes/todo.controller.js
+++ routes/todo.controller.js
@@ -1,10 +1,20 @@
 module.exports = function(app) {
   app.get('/todo', hello) 
+  app.get('/index', index) 
 }
 
 const hello = (req, res, next) => {
   try {
     res.render('index', { title: 'Yo yo yo this a app created by Anu' })
+  } 
+  catch (e) {
+    next(err)
+  }
+}
+
+const index = (req, res, next) => {
+  try {
+    res.render('fun', { title: 'This is an index page' })
   } 
   catch (e) {
     next(err)
